8ecd837fc4cbd1bcc326a0367e50aa14d5cec8fa
AD007xD <ad007xd@gmail.com>
2023-01-25 12:29:15 +0530

    Finished Window Object Methods

---
+++ Day 3/window-object.js
@@ -0,0 +1,7 @@
+alert('This is an alert');
+
+const value = prompt('This is a prompt', ' Enter a value');
+console.log(value);
+
+const agree = confirm('This is a confirmation', 'Yes or no?');
+console.log(agree);
