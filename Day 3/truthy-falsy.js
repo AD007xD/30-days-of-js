71b2d4ae11f87f4c21257ce3b1be5b45b787e6fa
AD007xD <ad007xd@gmail.com>
2023-01-25 12:28:51 +0530

    Finished Truthy-Falsy

---
+++ Day 3/truthy-falsy.js
@@ -0,0 +1,16 @@
+//!Booleans
+
+//!! Truthy and Falsy
+
+//` Truthy
+//* All numbers
+//* All strings
+//* true
+
+//` Falsy
+//* 0
+//* null
+//* undefined
+//* NaN
+//* false
+//* '' (empty string)
