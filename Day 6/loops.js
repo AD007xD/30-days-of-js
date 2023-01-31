b1070d2079d48333d24e832e1441936e77af97eb
AD007xD <ad007xd@gmail.com>
2023-01-31 23:03:46 +0530

    Started Loops

---
+++ Day 6/loops.js
@@ -0,0 +1,10 @@
+const countries = ['Australia', 'Canada', 'China', 'Mongolia', 'Japan']
+
+//!! For loop
+
+//` for(iterator; condition; change){statements}
+
+for(i = 0; i < 10; i++){
+    console.log(i);
+}
+
