7add73828b6c7ce22e5ee58b009969c9bc28c3b2
AD007xD <92644369+AD007xD@users.noreply.github.com>
2023-02-14 14:14:06 +0530

    Finished loops

--- Day 6/loops.js
+++ Day 6/loops.js
@@ -1,10 +1,27 @@
-const countries = ['Australia', 'Canada', 'China', 'Mongolia', 'Japan']
+const countries = ['Australia', 'Canada', 'China', 'Japan']
+const dict = {
+    Australia: 'Canberra',
+    Canada: 'Ottawa',
+    China: 'Beijing',
+    Japan: 'Tokyo'
+}
 
 //!! For loop
-
 //` for(iterator; condition; change){statements}
 
 for(i = 0; i < 10; i++){
     console.log(i);
 }
 
+const capitalCountries = []
+
+//` for of loop
+for(country of countries){
+    capitalCountries.push(country.toUpperCase())
+}
+console.log(capitalCountries);
+
+//` for in loop
+for(country in dict){
+    console.log(`${country}: ${dict[country]}`);
+}
